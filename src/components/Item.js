43e0e82ecfff1a6ef9dcc2c9e365e55ce4577eae
Pascal Odudigbo <odudigbopascal@gmail.com>
2022-07-18 19:10:50 +0300

    Done.

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [inOrOut, setInOrOut] = useState(true);
+
+  function handleInOrOut(){
+    setInOrOut((inOrOut) => !inOrOut);
+  }
+  const addOrRemoveItem = inOrOut ? "" : "in-cart";
+  const inCartOrNot = inOrOut ? "Add to Cart" : "Remove From Cart";
   return (
-    <li className="">
+    <li className= {addOrRemoveItem}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleInOrOut}>{inCartOrNot}</button>
     </li>
   );
 }
